4487ca9fd7f1c6422091c86cafa53f3573cf06d5
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2023-02-26 18:15:44 +0100

    Create scenarioAll.sh

---
+++ OnDemandSDNSlices/scenarios/scenarioAll.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/bash
+
+clear
+
+printf "[INFO] Loading Scenario 1\n\n"
+
+printf "[INFO] Setting up switches...\n\n"
+
+# Switch 1
+printf "Switch 1:\n"
+sudo ovs-vsctl -- \
+set port s1-eth1 qos=@newqos -- \
+set port s1-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:12=@1q queues:34=@2q queues:56=@3q queues:78=@4q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=25000000 -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=25000000 -- \
+--id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=75000000 -- \
+--id=@4q create queue other-config:min-rate=1000000 other-config:max-rate=75000000
+
+# Switch 2
+printf "\nSwitch 2:\n"
+sudo ovs-vsctl -- \
+set port s2-eth1 qos=@newqos -- \
+set port s2-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:12=@1q queues:56=@3q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=25000000 -- \
+--id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=75000000
+
+# Switch 3
+printf "\nSwitch 3:\n"
+sudo ovs-vsctl -- \
+set port s3-eth1 qos=@newqos -- \
+set port s3-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:34=@2q queues:78=@4q -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=25000000
+--id=@4q create queue other-config:min-rate=1000000 other-config:max-rate=75000000
+
+# Switch 4
+printf "\nSwitch 4:\n"
+sudo ovs-vsctl -- \
+set port s4-eth1 qos=@newqos -- \
+set port s4-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:12=@1q queues:34=@2q queues:56=@3q queues:78=@4q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=25000000 -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=25000000 -- \
+--id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=75000000 -- \
+--id=@4q create queue other-config:min-rate=1000000 other-config:max-rate=75000000
+
+# Creating links
+printf "\n[INFO] Creating links..."
+
+# Switch 1
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.5,idle_timeout=0,actions=set_queue:56,output:1
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.6,idle_timeout=0,actions=set_queue:56,output:5
+
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:12,output:1
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,idle_timeout=0,actions=set_queue:12,output:3
+
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.4,idle_timeout=0,actions=set_queue:34,output:4
+
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.7,idle_timeout=0,actions=set_queue:78,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.8,idle_timeout=0,actions=set_queue:78,output:6
+
+# Switch 2
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.5,idle_timeout=0,actions=set_queue:56,output:2
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.6,idle_timeout=0,actions=set_queue:56,output:1
+
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:12,output:2
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.2,idle_timeout=0,actions=set_queue:12,output:1
+
+# Switch 3
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:2
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.4,idle_timeout=0,actions=set_queue:34,output:1
+
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.7,idle_timeout=0,actions=set_queue:78,output:2
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.8,idle_timeout=0,actions=set_queue:78,output:1
+
+# Switch 4
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.5,idle_timeout=0,actions=set_queue:56,output:5
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.6,idle_timeout=0,actions=set_queue:56,output:1
+
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:12,output:3
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.2,idle_timeout=0,actions=set_queue:12,output:1
+
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:4
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.4,idle_timeout=0,actions=set_queue:34,output:2
+
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,idle_timeout=0,actions=set_queue:78,output:6
+sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,idle_timeout=0,actions=set_queue:78,output:2
+
+printf "OK\n\n"

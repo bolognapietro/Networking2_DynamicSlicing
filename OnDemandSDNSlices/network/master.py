152a338186503f852fad02ca8b6823554e524f10
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2023-02-28 09:50:29 +0100

    Update master.py

--- OnDemandSDNSlices/network/master.py
+++ OnDemandSDNSlices/network/master.py
@@ -16,14 +16,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((params.BRIDGE_HOST, params.BRIDGE_PORT))
 s.sendall(command.encode())
 
+s.setblocking(0)
+
 # Wait for response
+prev = False
+result = ""
+
 while True:
-    result = s.recv(1024)
 
-    if result != None:
-        break
+    try:
+        data = s.recv(1024)
+    except BlockingIOError: # Means that there is no data available
+        
+        # Close if i have already read something
+        if prev: 
+            break
 
-    sleep(0.5)
+        sleep(0.5)
+        continue
+    
+    # Store all the received data
+    if data != None:
+        result = result + data.decode()
+        prev = True
+
 
 # Close socket
 s.close()

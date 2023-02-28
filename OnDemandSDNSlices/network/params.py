c3295b75063e18c962671d8c6ce7eb3a17ca067a
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2023-02-28 01:16:18 +0100

    Create params.py

---
+++ OnDemandSDNSlices/network/params.py
@@ -0,0 +1,9 @@
+from os.path import join, dirname, realpath
+
+CONTROLLER_NAME = "c1"
+CONTROLLER_IP = "127.0.0.1"
+CONTROLLER_PORT = 6633
+CONTROLLER_PATH = join(dirname(realpath(__file__)),'controller.py')
+
+BRIDGE_HOST = "localhost"
+BRIDGE_PORT = 6634

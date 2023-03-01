14e78d462d2616ca290034d03951a04e4898a29f
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2023-03-01 19:41:04 +0100

    Update WebApp.py

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -1,50 +1,50 @@
-from flask import Flask, jsonify, request, render_template
-import uuid
-from os import chdir, getcwd, system
-from os.path import realpath, isfile, dirname, join
+from flask import Flask, request, render_template
+from os import chdir, getcwd
+from os.path import realpath, dirname, join
 import json
-from datetime import datetime
-from time import time, sleep
-from threading import Lock
-from enum import Enum
-import subprocess
-import atexit
 
 import master
 
+# Set current path as working dir
 chdir(dirname(realpath(__file__)))
 
+# Init Flask app
 app = Flask(__name__, template_folder=join(getcwd(),"templates"), static_folder=join(getcwd(),"static"))
 
+# Home
 @app.route("/")
 def home():
     return render_template('home.html')
 
+# Switch on mininet network
 @app.route("/networkON")
 def networkON():
     master.exec_command("start")
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
+# Switch off mininet network
 @app.route("/networkOFF")
 def networkOFF():
     master.exec_command("stop")
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
+# Get network map
 @app.route("/mapNetworkScenarios")
 def mapNetworkScenarios():
     result = master.exec_command("mapNetworkScenarios")
     return json.dumps(result), 200, {'ContentType':'application/json'}
 
+# Check if the mininet network is online
 @app.route("/ping")
 def ping():
     result = master.exec_command("ping")
     return result, 200, {'ContentType':'application/json'}
 
+# Change scenario
 @app.route("/changeScenario",methods=['POST'])
 def changeScenario():
     master.exec_command(f"changeScenario={request.form['scenarioId']}")
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
+# Run server
 app.run()
-
-
